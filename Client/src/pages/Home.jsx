16a76618beff1b9b2e838f00f82995ce6e8f050c
scm <harshitvarshneyv@gmail.com>
2025-03-12 12:31:53 +0530

    All changes Done in Navbar

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
+   <div>
+         <h1>All-in-One Placement Hub – Everything you need to land your dream job.</h1>
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-6xl font-bold text-blue-600">Priyanshu Don</h1>
+    </div>  
     </div>
   );
 };
-
 export default Home;
