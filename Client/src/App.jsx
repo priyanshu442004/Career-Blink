bcf519da828643e09bb35fc6bcb151a63c0c82fd
priyanshu442004 <priyanshusharma442004@gmail.com>
2025-03-12 10:30:23 +0530

    ATS

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route,RouterProvider} from 'react-router-dom';
 import Home from './pages/Home';
-import ScanResume from './pages/ScanResume';
 import MockInterview from './pages/MockInterview';
 import PrepMaterial from './pages/PrepMaterial';
 import EnglishCommunication from './pages/EnglishCommunication'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Rootlayout from './layout/Rootlayout';
+import Ats_Checker from './pages/Ats_Checker';
 
 const App = () => {
 
@@ -15,7 +15,7 @@ const App = () => {
       createRoutesFromElements(
          <Route path='/' element={<Rootlayout />}>
          <Route index element={<Home />} />
-         <Route path='ScanResume' element={<ScanResume />} />  
+         <Route path='ATSChecker' element={<Ats_Checker />} />  
          <Route path='MockInterview' element={<MockInterview />} /> 
          <Route path='PrepMaterial' element={<PrepMaterial />} />
          <Route path='EnglishCommunication' element={<EnglishCommunication />} />   
