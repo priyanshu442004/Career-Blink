2be6b338ec46e64a4a9f0fa4410125968f070ad5
scm <harshitvarshneyv@gmail.com>
2025-03-16 13:07:34 +0530

    change in Home

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -5,7 +5,6 @@ const Home = () => {
    <div className='w-full flex flex-col justify-center items-center'>
          <h1>All-in-One Placement Hub – Everything you need to land your dream job.</h1>
     <div className="flex items-center justify-center h-screen bg-gray-100 w-full">
-      <h1 className="text-6xl font-bold text-blue-600">Priyanshu Don</h1>
     </div>  
     </div>
   );
