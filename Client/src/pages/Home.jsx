bf9a245e5128044d552e97a771d06477c5352e4f
scm <harshitvarshneyv@gmail.com>
2025-03-17 11:05:44 +0530

    photos changes Done in home

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-   <div className='w-full flex flex-col justify-center items-center'>
-         <h1>All-in-One Placement Hub – Everything you need to land your dream job.</h1>
-    <div className="flex items-center justify-center h-screen bg-gray-100 w-full">
+   <div>
+         <div>      
+           <h1>All-in-One Placement Hub – Everything you need to land your dream job.</h1>
+          </div>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-6xl font-bold text-blue-600">Tanvee Don</h1>
     </div>  
     </div>
   );
