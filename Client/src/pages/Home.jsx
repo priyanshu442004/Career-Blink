8356272262ebed7f766146a5546a6bc15d4583e5
priyanshu442004 <priyanshusharma442004@gmail.com>
2025-03-17 20:08:09 +0530

    h

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Home = () => {
   return (
-   <div>
-         <div className='flex flex-col justify-center items-center  bg-gradient-to-r from-blue-600 to-green-400 h-8 text-white'>      
+   <div className='bg-gray-100'>
+         <div className='flex flex-col justify-center items-center bg-gradient-to-r from-blue-600 to-green-400 h-8 text-white font-semibold '>      
            <h1>All-in-One Placement Hub – Everything you need to land your dream job.</h1>
           </div>
+          
     <div className="flex items-center justify-center h-screen bg-gray-100">
       
     </div>  
