be28c3c3f84aab1cc5c5cbd13d315f6dc54ef751
priyanshu442004 <priyanshusharma442004@gmail.com>
2025-03-17 11:15:05 +0530

    Allin one

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Home = () => {
   return (
    <div>
-         <div>      
+         <div className='flex flex-col justify-center items-center  bg-gradient-to-r from-blue-600 to-green-400 h-8 text-white'>      
            <h1>All-in-One Placement Hub – Everything you need to land your dream job.</h1>
           </div>
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-blue-600">Tanvee Don</h1>
+      
     </div>  
     </div>
   );
