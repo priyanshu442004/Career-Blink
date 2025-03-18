5bf0d94e9d90e45e6f23cb7020c099c1ddb055de
scm <harshitvarshneyv@gmail.com>
2025-03-18 12:12:39 +0530

    Ai Module has some change

--- Client/src/pages/AIMockInterview.jsx
+++ Client/src/pages/AIMockInterview.jsx
@@ -1,12 +1,19 @@
-// AIzaSyC4kZKYfxLI7vJF2tNTX91blLmzjqlgYUk
 import React from "react";
+import mock1 from "../assets/AIMOCK/mock1.jpg";
+const AIMockInterview = () => {
+  return (
+    <div className="relative flex items-center justify-center min-h-screen">
+      <img src={mock1} alt="" className="w-full h-full object-cover" />
 
-const AIMockInterview = () =>{
-    return (
-        <div>
-            AIMockInterview page
-        </div>
-    )
-}
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black text-3xl font-bold">
+        Get Ready for Your Next Interview <br/>
+        <div className="text-white">
+        Pick the position you are interviewing for, take an AI-powered practice 
+      interview, and get feedback on your performance. 100% free.
+      </div>
+      </div>
+    </div>
+  );
+};
 
-export default AIMockInterview
+export default AIMockInterview;
