9aa40377ad8226b1593d1170b94a9228a32f58b3
scm <harshitvarshneyv@gmail.com>
2025-03-23 13:17:58 +0530

    This is done

--- Client/src/pages/AIMockInterview.jsx
+++ Client/src/pages/AIMockInterview.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const AIMockInterview = () => {
+  const goToPage = useNavigate();
+  // Function to handle navigation
+  const handleNavigate1 = () => {
+    goToPage("/AIMockInterview/JavaInterview");
+  };
   return (
     <div className="overflow-x-hidden bg-[url('../src/assets/AIMOCK/mock1.jpg')] bg-cover bg-center h-screen flex flex-col items-center justify-center w-full">
-      
       {/* Main Heading */}
-      <div className="text-black text-6xl font-extrabold text-center w-full mt-20 leading-tight">
+      <div className="text-black text-6xl font-extrabold text-center w-full mt-80 leading-tight">
         Prepare for your next interview
-        <div className="text-gray-800 mt-7 text-lg font-medium max-w-3xl mx-auto">
-          Pick the position you are interviewing for, take an AI-powered practice
-          interview, and get feedback on your performance. 100% free.
+        <div className="text-black-800 mt-5 text-lg font-medium max-w-3xl mx-auto">
+          Pick the position you are interviewing for, take an AI-powered
+          practice interview, and get feedback on your performance. 100% free.
         </div>
       </div>
 
-      {/* AI Engineer Component */}
+      {/* JAVA Component */}
       <div className="mt-16 w-full max-w-xl px-4">
         <div className="border rounded-lg p-6 shadow-sm bg-blue-100 hover:shadow-md transition-all">
           <p className="text-sm text-black-500 mb-2">12 mins interview</p>
@@ -22,14 +27,14 @@ const AIMockInterview = () => {
 
           <div className="flex flex-wrap gap-2 mb-6">
             <span className="px-3 py-1 text-sm bg-white border rounded-md">
-              Core JAVA 
+              Core JAVA
             </span>
             <span className="px-3 py-1 text-sm bg-white border rounded-md">
               Spring Framework
             </span>
           </div>
 
-          <button className="w-full py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+          <button  onClick={handleNavigate1}className="w-full py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
             Take practice interview
           </button>
         </div>
@@ -50,12 +55,14 @@ const AIMockInterview = () => {
             </span>
           </div>
 
-          <button className="w-full py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
+          <button
+            className="w-full py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+          >
             Take practice interview
           </button>
         </div>
       </div>
-    </div>      
+    </div>
   );
 };
 
---
+++ Client/src/pages/AIMockInterview/JavaInterview.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const JavaInterview = () =>{
+    return(
+        <div>
+            <h1>Java interview</h1>
+        </div>
+    )
+}
+export default JavaInterview;
