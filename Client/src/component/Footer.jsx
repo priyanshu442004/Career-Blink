292fe3a7c94ef17a39484fa556a95220f7075224
scm <harshitvarshneyv@gmail.com>
2025-03-23 15:50:23 +0530

    See this now footer

--- Client/src/component/Footer.jsx
+++ Client/src/component/Footer.jsx
@@ -25,29 +25,29 @@ const Footer = () => {
         <div>
           <h2 className="text-2xl font-semibold mb-6 text-black">Useful Links</h2>
           <Link to="/ATSChecker" className="block hover:text-blue-600 mb-2">Scan Resume</Link>
-          <Link to="/Talent" className="block hover:text-blue-600 mb-2">AI Mock Interview</Link>
-          <Link to="/Talent" className="block hover:text-blue-600 mb-2">Interview Prepration</Link>
-          <Link to="/AIRecruiter" className="block hover:text-blue-600 mb-2">English Communication</Link>
+          <Link to="/" className="block hover:text-blue-600 mb-2">AI Mock Interview</Link>
+          <Link to="/=" className="block hover:text-blue-600 mb-2">Interview Prepration</Link>
+          <Link to="/" className="block hover:text-blue-600 mb-2">English Communication</Link>
         </div>
 
         {/* Resources Section */}
         <div>
           <h2 className="text-2xl font-semibold mb-6 text-black">Resources</h2>
-          <Link to="/ApplyNow" className="block hover:text-blue-600 mb-2">Resume Formatting Tips</Link>
-          <Link to="/InterviewPrep" className="block hover:text-blue-600 mb-2">Notes</Link>
-          <Link to="/InterviewPrep" className="block hover:text-blue-600 mb-2">Interview Tips</Link>
-          <Link to="/InterviewPrep" className="block hover:text-blue-600 mb-4">DSA</Link>
+          <Link to="/" className="block hover:text-blue-600 mb-2">Resume Formatting Tips</Link>
+          <Link to="/" className="block hover:text-blue-600 mb-2">Notes</Link>
+          <Link to="/" className="block hover:text-blue-600 mb-2">Interview Tips</Link>
+          <Link to="/" className="block hover:text-blue-600 mb-4">DSA</Link>
           <h3 className="text-lg font-medium mb-2 text-black">Skills To Upgrade</h3>
-          <Link to="/AIAutomation" className="block hover:text-blue-600 mb-2">English Communication</Link>
-          <Link to="/SourcingVetting" className="block hover:text-blue-600">Body Language</Link>
+          <Link to="/" className="block hover:text-blue-600 mb-2">English Communication</Link>
+          <Link to="/" className="block hover:text-blue-600">Body Language</Link>
         </div>
 
         {/* Company Section */}
         <div>
-          <h2 className="text-2xl font-semibold mb-6 text-black">Company</h2>
-          <Link to="/AboutUs" className="block hover:text-blue-600 mb-2">About Us</Link>
-          <Link to="/Blog" className="block hover:text-blue-600 mb-2">Why Choose Us</Link>
-          <Link to="/CaseStudies" className="block hover:text-blue-600 mb-4">Case Studies</Link>
+          <h2 className="text-2xl font-semibold mb-6 text-black">Get In touch</h2>
+          <h1  className="block hover:text-blue-600 mb-2">harshitvarshneyv2@gmail.com</h1>
+          <h1 className="block hover:text-blue-600 mb-2">priyanshusharma442004@gmail.com</h1>
+          <h1 className="block hover:text-blue-600 mb-4">8445544116</h1>
         </div>
       </div>
 
