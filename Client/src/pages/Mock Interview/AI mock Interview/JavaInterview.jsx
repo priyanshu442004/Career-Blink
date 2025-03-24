9d05c0b5930a72b450ec572fa29642e19c76ca6a
scm <harshitvarshneyv@gmail.com>
2025-03-24 13:09:08 +0530

    Java Page updated

--- Client/src/pages/Mock Interview/AI mock Interview/JavaInterview.jsx
+++ Client/src/pages/Mock Interview/AI mock Interview/JavaInterview.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const JavaInterview = () =>{
     return (
-        <div>
-            <h1>Java</h1>
+            <div className="overflow-x-hidden bg-[url('../src/assets/AIMOCK/mock1.jpg')] bg-cover bg-center h-screen flex flex-col items-center justify-center w-full">
+      {/* Main Heading */}
+      <div className="text-black text-6xl font-extrabold text-center w-full mt-1 leading-tight">
+        Prepare for your next interview
+        <div className="text-black-800 mt-5 text-lg font-medium max-w-3xl mx-auto">
+          Take an AI-powered Java
+          practice interview, and get feedback on your performance. 100% free.
+          Let's Make you confident to crack Java Interview
+        </div>
+      </div>
         </div>
     )
 }
