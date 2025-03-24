991db1eb3332f9aeb761462e447c8abb017c03e8
priyanshu442004 <priyanshusharma442004@gmail.com>
2025-03-24 21:54:04 +0530

    Footer alignment

--- Client/src/layout/Rootlayout.jsx
+++ Client/src/layout/Rootlayout.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import NavBar from "../component/NavBar";
-import Footer from "../component/footer";
 import { Outlet,useLocation } from "react-router-dom";
+import Footer from "../component/Footer";
 
 const Rootlayout = () =>{
  
     const location = useLocation();
     return(
-        <div>
-          <NavBar />
-          <Outlet/>
-           <Footer className="mt-auto"/>
+        <div className="flex flex-col min-h-screen">
+          <NavBar/>
+          <main className="flex-grow">  
+        <Outlet />
+      </main>
+           <Footer />
         </div>
     )
 }
