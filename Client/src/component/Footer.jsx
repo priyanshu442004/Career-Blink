9487b9f3cb2931d4f8fa6d47b5f9578d4550369e
scm <harshitvarshneyv@gmail.com>
2025-03-24 12:52:36 +0530

    Footer has been Updated

--- Client/src/component/Footer.jsx
+++ Client/src/component/Footer.jsx
@@ -25,20 +25,19 @@ const Footer = () => {
         <div>
           <h2 className="text-2xl font-semibold mb-6 text-black">Useful Links</h2>
           <Link to="/ATSChecker" className="block hover:text-blue-600 mb-2">Scan Resume</Link>
-          <Link to="/" className="block hover:text-blue-600 mb-2">AI Mock Interview</Link>
-          <Link to="/=" className="block hover:text-blue-600 mb-2">Interview Prepration</Link>
-          <Link to="/" className="block hover:text-blue-600 mb-2">English Communication</Link>
+          <Link to="/AIMockInterview" className="block hover:text-blue-600 mb-2">AI Mock Interview</Link>
+          <Link to="/InterviewPractice" className="block hover:text-blue-600 mb-2">Interview Prepration</Link>
+          <Link to="/EnglishCommunication" className="block hover:text-blue-600 mb-2">English Communication</Link>
         </div>
 
         {/* Resources Section */}
         <div>
           <h2 className="text-2xl font-semibold mb-6 text-black">Resources</h2>
-          <Link to="/" className="block hover:text-blue-600 mb-2">Resume Formatting Tips</Link>
-          <Link to="/" className="block hover:text-blue-600 mb-2">Notes</Link>
-          <Link to="/" className="block hover:text-blue-600 mb-2">Interview Tips</Link>
-          <Link to="/" className="block hover:text-blue-600 mb-4">DSA</Link>
+          <Link to="/FormattingTips" className="block hover:text-blue-600 mb-2">Resume Formatting Tips</Link>
+          <Link to="/Notes" className="block hover:text-blue-600 mb-2">Notes</Link>
+          <Link to="/InterviewTips" className="block hover:text-blue-600 mb-2">Interview Tips</Link>
+          <Link to="/DSA" className="block hover:text-blue-600 mb-4">DSA</Link>
           <h3 className="text-lg font-medium mb-2 text-black">Skills To Upgrade</h3>
-          <Link to="/" className="block hover:text-blue-600 mb-2">English Communication</Link>
           <Link to="/" className="block hover:text-blue-600">Body Language</Link>
         </div>
 
