7b389a1c9c6bfa97b513fafbbbca4c9e92101401
scm <harshitvarshneyv@gmail.com>
2025-03-26 20:47:36 +0530

    Spring boot has been added.Now the time to add backend

---
+++ Server/carrierblink/src/main/java/com/backend/carrierblink/CarrierblinkApplication.java
@@ -0,0 +1,13 @@
+package com.backend.carrierblink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrierblinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrierblinkApplication.class, args);
+	}
+
+}
---
+++ Server/carrierblink/src/test/java/com/backend/carrierblink/CarrierblinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.carrierblink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrierblinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
