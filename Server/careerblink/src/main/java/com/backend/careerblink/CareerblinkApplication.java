cf6f9b18673a41527eaa96cc5ab495d979522165
scm <harshitvarshneyv@gmail.com>
2025-03-28 15:01:38 +0530

    Backend added

---
+++ Server/careerblink/src/main/java/com/backend/careerblink/CareerblinkApplication.java
@@ -0,0 +1,13 @@
+package com.backend.careerblink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerblinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerblinkApplication.class, args);
+	}
+
+}
